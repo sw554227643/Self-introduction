d4f2ac657a5d62c52353b71fd5a27ca94abc482e
sw <554227643@qq.com>
2019-05-28 18:05:54 +0800

    添加了显示默认标题和默认图片的功能

--- gallery/models.py
+++ gallery/models.py
@@ -4,7 +4,11 @@ from django.db import models
 
 
 class Gallery(models.Model):
-    description = models.CharField(max_length=100)   #描述功能 最大限制字数100
+    description = models.CharField(default='在这里写作品的简介',max_length=100)   #描述功能 最大限制字数100
     #创建后在admin中注册
+    image = models.ImageField(default = 'default.png',upload_to='images/') #默认图片默认路径
+    title = models.CharField(default = '作品标题',max_length=50)
 
+    def __str__(self):
+        return self.title
 
